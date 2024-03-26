fb2ce8aa7f0058f063177a89b551cda03e59efaa
DeokJong <novelss444@jbnu.ac.kr>
2024-03-26 23:09:59 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
+//ReactDOM.render의 역할은 DOM 내부의 리액트 컴포넌트를 렌더링하겠다라는 것을 의미함.
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
